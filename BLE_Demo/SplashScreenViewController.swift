1ec2f687075019764aba9a3bab5db5a75ba3e9b3
Kerri <kzalbak@gmail.com>
2019-03-18 14:51:49 -0400

    Firebase installation

---
+++ BLE_Demo/SplashScreenViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SplashScreenViewController.swift
+//  gaiaDemo
+//
+//  Created by User on 3/18/19.
+//  Copyright © 2019 IQVIS. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func moveBlue(_ sender: Any) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "navScreen") 
+        self.present(nextViewController, animated:true, completion:nil)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
